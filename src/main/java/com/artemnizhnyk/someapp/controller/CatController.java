c1efeb6271b9b8ef19a1189b5b0b80b475b5115a
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-01-18 21:01:58 +0100

    All rest api mathods were added and some old methods deleted or updated

--- src/main/java/com/artemnizhnyk/someapp/controller/CatController.java
+++ src/main/java/com/artemnizhnyk/someapp/controller/CatController.java
@@ -5,14 +5,37 @@ import com.artemnizhnyk.someapp.service.CatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
 public class CatController {
 
     private final CatService catService;
-   @PostMapping("/add-cat")
-    public void addCat(@RequestBody CatDto catDto) {
-        catService.saveCat(catDto);
-   }
+
+    @GetMapping("/cats")
+    public List<CatDto> findAll() {
+        return catService.findAll();
+    }
+
+    @GetMapping("/cat/{cat_id}")
+    public CatDto findById(@PathVariable("cat_id") final Long catId) {
+        return catService.findById(catId);
+    }
+
+    @PostMapping("/cat")
+    public CatDto add(@RequestBody CatDto catDto) {
+        return catService.save(catDto);
+    }
+
+    @PutMapping("/cat")
+    public CatDto update(@RequestBody CatDto catDto) {
+        return catService.update(catDto);
+    }
+
+    @DeleteMapping("/cat/{cat_id}")
+    public String deleteById(@PathVariable("cat_id") final Long catId) {
+        return catService.delete(catId);
+    }
 }
--- src/main/java/com/artemnizhnyk/someapp/service/CatService.java
+++ src/main/java/com/artemnizhnyk/someapp/service/CatService.java
@@ -9,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -17,9 +20,43 @@ public class CatService {
     private final CatRepository catRepository;
     private final CatMapper catMapper = Mappers.getMapper(CatMapper.class);
 
-    public void saveCat(final CatDto catDto) {
+    public CatDto save(final CatDto catDto) {
         Cat cat = catMapper.toEntity(catDto);
         catRepository.save(cat);
-        log.info(String.format("Cat with id %d and name %s was saved", cat.getId(), cat.getName()));
+        log.info(String.format("Cat with id \"%d\" and name \"%s\" was saved", cat.getId(), cat.getName()));
+        return catMapper.toDto(cat);
+    }
+
+    public List<CatDto> findAll() {
+        return catRepository.findAll()
+                .stream()
+                .map(catMapper::toDto)
+                .toList();
+    }
+
+    public CatDto findById(final Long catId) {
+        return catRepository.findById(catId)
+                .map(catMapper::toDto)
+                .orElse(new CatDto());
+    }
+
+    public String delete(final Long catId) {
+        if (!catRepository.existsById(catId)) {
+            return String.format("Cat with id \"%d\" doesn't exist in DB", catId);
+        }
+        catRepository.deleteById(catId);
+        log.info(String.format("Cat with id \"%d\" was deleted", catId));
+        return "Cat was successfully deleted";
+    }
+
+    public CatDto update(final CatDto catDto) {
+        Optional<Cat> optionalCat = catRepository.findById(catDto.getId());
+        Cat cat = optionalCat.orElse(new Cat());
+        cat.setName(catDto.getName());
+        cat.setAge(catDto.getAge());
+        cat.setWeight(catDto.getWeight());
+        catRepository.save(cat);
+        log.info(String.format("Cat with id \"%d\" and name \"%s\" was updated", cat.getId(), cat.getName()));
+        return catMapper.toDto(cat);
     }
 }
